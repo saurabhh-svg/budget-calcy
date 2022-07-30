45a097dddeb09cbc5dc25bec749bbd78105497a4
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-30 16:30:00 +0530

    adding states

--- src/App.js
+++ src/App.js
@@ -12,14 +12,37 @@ const intialExpenses = [
 ];
 
 function App() {
+  //** State Values **//
+  //All expenses, add expense
   const [expenses, setExpenses] = useState(intialExpenses);
-
+  //single expense
+  const [charge, setCharge] = useState("");
+  //single amount
+  const [amount, setAmount] = useState("");
+  // ** Functionality **//
+  const handleCharge = (e) => {
+    console.log(`charge: ${e.target.value}`);
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    console.log(`amount: ${e.target.value}`);
+    setAmount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <Alert />
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { MdSend } from "react-icons/md";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit,
+}) => {
   return (
-    <div>ExpenseForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+            value={charge}
+            onChange={handleCharge}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 1000"
+            value={amount}
+            onChange={handleAmount}
+          ></input>
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        Submit
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
