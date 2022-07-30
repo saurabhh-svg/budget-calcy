cee0c0540813cb4858006161637e3ed604ada234
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-30 16:04:44 +0530

    expense list creation

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const ExpenseItem = () => {
+import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
+const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
   return (
-    <div>ExpenseItem</div>
-  )
-}
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">{amount}</span>
+      </div>
+      <div>
+        <button className="edit-btn" aria-label="edit button">
+          <MdEdit />
+        </button>
+        <button className="clear-btn" aria-label="delete button">
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,22 @@
 import React from "react";
-import ExpenseItem from "./ExpenseItem";
-
-const ExpenseList = () => {
+import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+const ExpenseList = ({ expenses }) => {
   return (
-    <div>
-      ExpenseList
-      <ExpenseItem />
-    </div>
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return <Item key={expense.id} expense={expense} />;
+        })}
+      </ul>
+       
+      {expenses.length > 0 && (
+        <button className="btn">
+          Clear expenses
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
   );
 };
 
